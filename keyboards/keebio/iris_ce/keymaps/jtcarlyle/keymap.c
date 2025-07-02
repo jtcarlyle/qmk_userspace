0dee8b01c8ed1a251b5864c3677922bd58e8dea2
John Carlyle <john.t.carlyle@gmail.com>
2025-07-02 21:00:13 +0900

    Swap language keys

--- keyboards/keebio/iris_ce/keymaps/jtcarlyle/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/jtcarlyle/keymap.c
@@ -15,7 +15,7 @@ enum custom_layers {
 #define ST_ENT  RSFT_T(KC_ENT)
 /* #define CT_ENT  LCTL_T(KC_ENT) */
 #define CT_EQL  LCTL_T(KC_EQL)
-#define CT_LNG1  RCTL_T(KC_LNG1)
+#define CT_LNG2  RCTL_T(KC_LNG2)
 #define LO_TAB LT(_LOWER, KC_TAB)
 #define RA_BSPC LT(_RAISE, KC_BSPC)
 #define AT_ESC  LALT_T(KC_ESC)
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_DVORAK] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LNG2,
+     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LNG1,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_BSLS, KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                               KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LGUI, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_LBRC,          KC_RBRC, KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RGUI,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                     CT_EQL, ST_SPC,  LO_TAB,                    RA_BSPC, ST_ENT,  CT_LNG1
+                                     CT_EQL, ST_SPC,  LO_TAB,                    RA_BSPC, ST_ENT,  CT_LNG2
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
