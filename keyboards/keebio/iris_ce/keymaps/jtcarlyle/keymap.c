561e635eee5192fd5de02bbcba32a71d8a917e28
John Carlyle <john.t.carlyle@gmail.com>
2025-07-02 16:52:00 +0900

    Revert back

--- keyboards/keebio/iris_ce/keymaps/jtcarlyle/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/jtcarlyle/keymap.c
@@ -14,6 +14,7 @@ enum custom_layers {
 #define ST_SPC  LSFT_T(KC_SPC)
 #define ST_ENT  RSFT_T(KC_ENT)
 #define CT_ENT  LCTL_T(KC_ENT)
+#define CT_F13  LCTL_T(KC_F13)
 #define LO_TAB LT(_LOWER, KC_TAB)
 #define RA_BSPC LT(_RAISE, KC_BSPC)
 #define AT_ESC  LALT_T(KC_ESC)
@@ -54,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LGUI, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_LBRC,          KC_RBRC, KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RGUI,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                     CT_ENT, ST_SPC,  LO_TAB,                    RA_BSPC, KC_RSFT,  KC_RCTL
+                                     CT_F13, ST_SPC,  LO_TAB,                    RA_BSPC, ST_ENT,  KC_RCTL
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
